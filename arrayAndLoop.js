1b2a0d26d40fabeedc3cb513cc170896e35d0d30
Tranfersafety <ratthaphumi.safe@gmail.com>
2024-05-15 14:49:13 +0700

    finished

---
+++ arrayAndLoop.js
@@ -0,0 +1,7 @@
+const peopleInLine = ['Sofia','David',"Juan"]
+peopleInLine.push("Sara","Augustin")
+peopleInLine.shift()
+console.log(peopleInLine)
+peopleInLine.splice(1,0,'Renata')
+peopleInLine.push('Elena')
+console.log(peopleInLine)
